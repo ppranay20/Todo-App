29425f22ce06331753cf84165316b33947441e69
ppranay-20 <pranay@omaratechnologies.com>
2024-10-27 17:30:40 +0530

    changed padding

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -11,7 +11,7 @@ interface todoProps{
 
 export default function Todo({title,isCompleted,id,deleteTodo,updateTodo,dark} : todoProps){
     return(
-        <div className="grid grid-cols-10 justify-items-start py-3 border bg-gray-100 w-[95%] my-4 rounded-lg m-auto text-sm dark:bg-[#3d3d43] dark:border-[#3d3d43]">
+        <div className="grid grid-cols-10 justify-items-start py-2 border bg-gray-100 w-[95%] my-4 rounded-lg m-auto text-sm dark:bg-[#3d3d43] dark:border-[#3d3d43]">
             <div>
                 <input onChange={(e) => e.target.checked ? updateTodo(id) : null} type="checkbox" className="col-span-1 h-[14px] w-4 mt-[3px] ml-2 rounded-sm border checked:accent-red-600 checked:hidden dark:checked:accent-black" size={2}   />
             </div>
